9f696f25a53ff1fec1d78eef1718440be80c37b6
Lkavalci <lutfikavalci@gmail.com>
2023-06-17 21:36:27 +0300

    230617

---
+++ src/test/java/test/C06_Post_ResponseBodyTesti.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.junit.Test;
+
+public class C06_Post_ResponseBodyTesti {
+
+    /*  https://jsonplaceholder.typicode.com/posts
+         url’ine asagidaki body ile bir POST request gonderdigimizde
+        {
+        "title":"API",
+        "body":"API ogrenmek ne guzel",
+        "userId":10,
+        }
+        donen Response’un,
+        status code’unun 201,
+        ve content type’inin application/json
+        ve Response Body'sindeki,
+        "title"'in "API" oldugunu
+        "userId" degerinin 100'den kucuk oldugunu
+        "body" nin "API" kelimesi icerdigini
+        test edin.
+      */
+    @Test
+    public void post01(){
+
+
+    }
+
+}
---
+++ src/test/java/test/C07_Get_BodyTekrarlardanKurtulma.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.junit.Test;
+
+public class C07_Get_BodyTekrarlardanKurtulma {
+
+ /*
+                https://restful-booker.herokuapp.com/booking/10 url’ine
+                bir GET request gonderdigimizde donen Response’un,
+                status code’unun 200,
+                ve content type’inin application/json; charset=utf-8,
+                ve response body’sindeki
+                    "firstname“in,"Jim",
+                    ve "lastname“in, "Wilson",
+                    ve "totalprice“in, 609,
+                    ve "depositpaid“in,false,
+                    ve "additionalneeds“in,"Breakfast"
+                oldugunu test edin
+         */
+
+    @Test
+    public void get01(){
+
+        // 1- Url hazirla
+
+
+
+    }
+}
